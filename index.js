0f0c41f5fe39ea7fb5fa8f672ddfd1be003caf14
A-HK <kahani.0142@gmail.com>
2023-03-29 02:44:49 +0530

    env change

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-console.log(process.env.OPENAI_API_KEY);
-
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });  
